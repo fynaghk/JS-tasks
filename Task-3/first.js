55699ca080a799d928b5f393352261e7cd90e312
fynaghk <n.fidan_15@mail.ru>
2021-01-22 01:36:56 +0400

    Done!

--- Task-3/first.js
+++ Task-3/first.js
@@ -1,6 +1,18 @@
-var arr = [1, 5, 8, 21, 4];
-arr.sort(function(a, b) {
-  return b-a;
-});
+var arr = [5, 1, 8, 21, 4];
+var newArr=[];
+var max=0;
 
-console.log(arr[0]);
+for (var i = 0; i < arr.length; i++) {
+  for (var j = i+1; j < arr.length; j++) {
+     if(arr[i] > arr[j]) {
+         max = arr[i];
+         arr[i] = arr[j];
+         arr[j] = max;
+     }
+  }
+}
+
+
+for (var i = 0; i < arr.length; i++) {
+  console.log(arr[i] + " ");
+}
