617966b9f99d62737e8c5f34e7893a589e9e1e5b
fynaghk <n.fidan_15@mail.ru>
2021-02-05 13:05:52 +0400

    Done!

--- Task-4/index.js
+++ Task-4/index.js
@@ -6,7 +6,7 @@ const button = document.getElementById("btn");
 
 const myDiv = document.getElementById("main");
 
-button.addEventListener("click",function(e){
+button.addEventListener("click",function(){
   myDiv.style.backgroundColor = color.value;
   myDiv.style.width = width.value;
   myDiv.style.height = height.value;
