95c986350f6b1fb56439338a544236ee2e37861c
fynaghk <n.fidan_15@mail.ru>
2021-02-05 13:05:21 +0400

    Done!

--- Task-4/index.js
+++ Task-4/index.js
@@ -1,1 +1,13 @@
-console.log("salam")
+const color = document.getElementById("color");
+const width = document.getElementById("width");
+const height = document.getElementById("height");
+
+const button = document.getElementById("btn");
+
+const myDiv = document.getElementById("main");
+
+button.addEventListener("click",function(e){
+  myDiv.style.backgroundColor = color.value;
+  myDiv.style.width = width.value;
+  myDiv.style.height = height.value;
+})
