e5550140dc405970da1d16060562e6b39914d335
fynaghk <n.fidan_15@mail.ru>
2021-02-09 20:02:32 +0400

    Done!

--- Task-6/index.js
+++ Task-6/index.js
@@ -4,13 +4,10 @@ var swiper = new Swiper('.swiper-container', {
   centeredSlides: true,
   slidesPerView: 'auto',
   coverflowEffect: {
-    rotate: 50,
+    rotate: 0,
     stretch: 0,
-    depth: 100,
+    depth: 0,
     modifier: 1,
     slideShadows: true,
-  },
-  pagination: {
-    el: '.swiper-pagination',
   },
 });
